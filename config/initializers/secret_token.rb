c0db24ee68ceabca1d29846f78e51b45f0c2ec71
Marvyn Orellana <marvynro1@gmail.com>
2015-09-23 18:04:00 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Urbo::Application.config.secret_key_base = '40d004731571052ee2616dbb77645f039f06ec3ff7e6c3915b9318709426074a462a3fffac8a3d6348e490e8d0698ec5d66fbc0f96f16029153c6b9288d67d82'
